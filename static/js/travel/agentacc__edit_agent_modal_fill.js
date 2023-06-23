f9b245276a8e92a6c0d0b244d28efa8b31c235dd
fakhruddin-vsbizz <fakhruddinvsbizz@gmail.com>
2023-06-23 17:57:50 +0530

    integrated put request for updating name, email, password. Deactivate WIP

--- static/js/travel/agentacc__edit_agent_modal_fill.js
+++ static/js/travel/agentacc__edit_agent_modal_fill.js
@@ -8,35 +8,98 @@ function fillAndPopAgentEditModal(id, first_name, last_name, email) {
     UIkit.modal(modal_instance).show();
 }
 
-function editNames(csrf_token) {
-    
-    first_name = document.getElementById('first_name').value;
-    last_name = document.getElementById('last_name').value;
+function dummyAlert(){
+    alert('console chala mast')
+}
+
+function editTravelAgent(csrf_token, tag) {
+
+    var pushable_data = null;
+
+    // form values fetched
+    first_name = document.getElementById('edit_first_name').value;
+    last_name = document.getElementById('edit_last_name').value;
+    email = document.getElementById('edit_email').value;
+    password = document.getElementById('edit_password').value;
+    conf_password = document.getElementById('conf_password').value;
+
+    // primary key of the employee
     id = document.getElementById('id_employee').value;
 
+    // api gate switch to allow to run API
     var is_api_go = false;
 
-    if(validateField(first_name) && validateField(last_name)){
-        is_api_go = true;
-    }else{
-        is_api_go = false;
-        alertbox.innerHTML = "Seems either of the field is empty or null. Please check the fields and try again";
-    }
-
-    if(is_api_go){
-        $.ajax({
-            url: 'http://localhost:8000/travel/api/edit_travel_agent_details',
-            type: 'PUT',
-            data: {
+    // switcher for the data to be returned
+    switch (tag) {
+        case "names":
+            pushable_data = {
                 csrfmiddlewaretoken: csrf_token,
                 "id": parseInt(id),
                 "first_name": first_name,
                 "last_name": last_name,
-            },
+            }
+
+            if(validateField(first_name) && validateField(last_name)){
+                is_api_go = true;
+            }else{
+                is_api_go = false;
+                alertbox.innerHTML = "Seems either of the field is empty or null. Please check the fields and try again";
+            }
+
+            break;
+        
+        case "email":
+            pushable_data = {
+                csrfmiddlewaretoken: csrf_token,
+                "id": parseInt(id),
+                "email": email
+            }
+
+            if(validateField(email)){
+                is_api_go = true;
+            }else{
+                is_api_go = false;
+                alertbox.innerHTML = "Seems either of the field is empty or null. Please check the fields and try again";
+            }
+
+            break;
+        
+        case "password":
+            pushable_data = {
+                csrfmiddlewaretoken: csrf_token,
+                "id": parseInt(id),
+                "password": password
+            }
+
+            if(validateField(password) && validateField(conf_password) && arePasswordSame(password, conf_password)){
+                is_api_go = true;
+            }else{
+                is_api_go = false;
+                alertbox.innerHTML = "Seems either of the field is empty or null. Please check the fields and try again";
+                
+            }
+
+            break;
+
+        default:
+            break;
+    }
+
+    // alertbox for showing alerts
+    alertbox = document.getElementById('edit_alertbox');
+    
+ 
+    // if variable true, api runs
+    if(is_api_go){
+        console.log('api run mast')
+        $.ajax({
+            url: 'http://localhost:8000/travel/api/edit_travel_agent_details',
+            type: 'PUT',
+            data: pushable_data,
             success: function(data){
-                console.log('succes sdata', data)
-                alertbox.innerHTML = data['response_message'];
-                // window.location.reload();
+                console.log('api run response')
+                alertbox.innerHTML = data['message'];
+                window.location.reload();
             },
             error: function(jqXHR, exception){
                 console.log(jqXHR, ' | ', exception);
@@ -44,8 +107,19 @@ function editNames(csrf_token) {
             },        
         });
     }else{
-        alertbox.innerHTML = "Some server error has occured. Please contact developer@vsbizz.com for more info.";
+        alertbox.innerHTML = "Seems either of the field is empty or null. Please check the fields and try again";
     }
+}
 
+// validate null, empty and space-only values
+function validateField(variant){
+    if(variant != "" && variant != null && variant != " "){
+        return true
+    }else{
+        return false
+    }
+}
 
+function arePasswordSame(pass, confpass) {
+    return pass == confpass;
 }
